e6f29787c59d4619992453aa2769a80470d9af1f
Luís <luisghfernandes@gmail.com>
2025-07-11 14:20:02 +0100

    chore: fixed drop automation

--- .scripts/update_changelog.sh
+++ .scripts/update_changelog.sh
@@ -5,8 +5,10 @@
 CHANGELOG_CONTENT="$1"
 
 if [ -z "$CHANGELOG_CONTENT" ]; then
-  echo "Error: No changelog content provided."
-  exit 1
+  echo "No changelog content provided. Writing default message."
+  printf "%s\n" "no changes identified" > CHANGELOG.md
+  echo "CHANGELOG.md updated with default message."
+  exit 0
 fi
 
 # Write the content to CHANGELOG.md in the project root
